dc49fe9ba6d71902b636e936676e58cb72f4b12e
anotherfre <1225977549@qq.com>
2021-10-09 15:33:14 +0800

    图片下载GUI

--- aliImage_GUI.py
+++ aliImage_GUI.py
@@ -6,14 +6,19 @@ from tkinter.filedialog import askdirectory
 import threading
 
 
-def download_image(text):
+def download_image(text, path, result_cnt):
     url_list = text.split('//')
     for url in url_list:
-        image_name = ''.join(random.sample(string.ascii_letters + string.digits, 8))
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            with open(image_name, 'wb') as f:
-                f.write(resp.content)
+        try:
+            url = 'http://' + url.rstrip('\n')
+            resp = requests.get(url)
+            if resp.status_code == 200:
+                image_name = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+                with open(path.get()+ '/' + image_name + '.jpg', 'wb') as f:
+                    f.write(resp.content)
+                    result_cnt.insert('end', url + ": successful"'\n')
+        except Exception as e:
+            result_cnt.insert('end', str(url) + 'failed: ' + str(e) + '\n')
     return True
 
 
@@ -31,11 +36,13 @@ class App:
         self.path = StringVar()
         Entry(root, textvariable=self.path).grid(row=0, column=1)
         Button(root, text='选择路径', command=self.select_path).grid(row=0, column=2)
-        self.cnt_text = Text(root, height=6, width=30).grid(row=1, columnspan=3)
+        self.cnt_text = Text(root, height=6, width=30)
+        self.cnt_text.grid(row=1, columnspan=3)
         Label(root, text='结果显示：').grid(row=2)
-        Text(root, height=6, width=30).grid(row=3, columnspan=3)
+        self.result_cnt = Text(root, height=6, width=30)
+        self.result_cnt.grid(row=3, columnspan=3)
 
-        Button(root, text='开始下载', command=thread_it(self.start_download())).grid(row=4, column=1)
+        Button(root, text='开始下载', command=lambda: thread_it(self.start_download)).grid(row=4, column=1)
         root.mainloop()
 
     def select_path(self):
@@ -47,10 +54,13 @@ class App:
         self.path.set(_path)
 
     def start_download(self):
+        self.result_cnt.delete(1.0, 'end')
         content = self.cnt_text.get('1.0', 'end')
         if not content:
             return False
-        result = download_image(content)
+        result = download_image(content, self.path, self.result_cnt)
+        if result:
+            self.result_cnt.insert('end', 'Image Download Finish')
 
 
 if __name__ == '__main__':
